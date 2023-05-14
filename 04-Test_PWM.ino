7d4f060c4acdf4c4785db3da984ea42f418ec1ea
Dr. Basuki Rahmat, S.Si, MT <48795125+bsrahmat@users.noreply.github.com>
2023-05-14 13:52:35 +0700

    Add files via upload

---
+++ 04-Test_PWM.ino
@@ -0,0 +1,38 @@
+/*************************************
+ * Program : Pengujian PWM
+ * Menggunakan Kit iTCLab
+ * Oleh : Tim io-t.net
+ * Surabaya, 16 April 2022
+ * ***********************************/
+ 
+// the number of the LED pin
+const int ledPin = 26;
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+ 
+void setup(){
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
+}
+ 
+void loop(){
+  // increase the LED brightness
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(20);
+  }
+
+  // decrease the LED brightness
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(20);
+  }
+}
